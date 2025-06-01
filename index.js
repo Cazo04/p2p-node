ee4d3dfaf634c78b37dd967141fd85aec7e89e5a
Cazo04 <nguyenkhang270604@gmail.com>
2025-06-01 13:03:22 +0000

    Refactor WebRTC configuration to define iceServers directly in the code

--- index.js
+++ index.js
@@ -105,7 +105,22 @@ socket.on('connect_error', (err) => {
 });
 
 // WebRTC configuration
-const webrtcConfig = config.webrtc;
+const webrtcConfig = {
+    iceServers: [
+        { urls: "stun:stun.l.google.com:19302" },
+        { urls: "stun:stun1.l.google.com:19302" },
+        { urls: "stun:stun2.l.google.com:19302" },
+        { urls: "stun:stun3.l.google.com:19302" },
+        
+        // Twilio
+        { urls: "stun:global.stun.twilio.com:3478?transport=udp" },
+        { urls: "stun:global.stun.twilio.com:443?transport=udp" },
+        
+        // OpenRelay
+        { urls: "stun:openrelay.metered.ca:80" },
+        { urls: "stun:openrelay.metered.ca:443" }
+    ]
+};
 
 // Node information
 const node_info = {
